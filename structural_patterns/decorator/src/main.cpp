b3fea97c2f9371faaba9b0577e76228d84226f66
ladianchad <qhrejddlvltm@gmail.com>
2022-03-21 17:27:38 +0900

    decorator

--- structural_patterns/decorator/src/main.cpp
+++ structural_patterns/decorator/src/main.cpp
@@ -1,9 +1,14 @@
 #include <decorator/time_decorator.hpp>
 #include <view.hpp>
+#include <adder.hpp>
 
 int main(int argc, char const *argv[])
 {
-    TimeDecorator<View> test;
-    test.execute(10);
+    View test1;
+    TimeDecorator<View> test2;
+    TimeDecorator<Adder> test3;
+    test1.execute(10);
+    test2.execute(10);
+    test3.execute(10,30);
     return 0;
 }
